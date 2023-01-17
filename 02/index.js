9c301b7f43ebe33b915585fcbd497efc23601f46
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-17 18:56:46 -0300

    Exercicio concluido

--- 02/index.js
+++ 02/index.js
@@ -2,4 +2,4 @@ let F = 73;
 let C = (F - 32) * 5 / 9;
 // 73 °F − 32) × 5/9 = 22,778 
 
-console.log(`73 Fahrenheit em Celsio é ${C}`);
+console.log(`73 Fahrenheit em Celsio é ${C.toFixed(2)}`);
---
+++ 03/index.js
@@ -0,0 +1,5 @@
+let valorTenis = 129.99;
+let dinheiro = 80;
+let desconto = dinheiro * 100 / valorTenis;
+
+console.log(`Eu preciso de ${desconto.toFixed(2)}% de desconto para comprar o tênis`);
